c9900f03b2ab8abe6e84886d56d156384f4bbd7b
Mateus Tassinari <mateus.ferreira@inflor.com.br>
2020-10-31 17:53:25 -0300

    otimização codigo no git

--- src/main/java/br/ufes/scap_tapestry/nucleo/aplicacao/UsuarioImp.java
+++ src/main/java/br/ufes/scap_tapestry/nucleo/aplicacao/UsuarioImp.java
@@ -6,41 +6,26 @@ public class UsuarioImp implements Usuario {
 
 	private Pessoa logado;
 	
-	/* (non-Javadoc)
-	 * @see br.ufes.scap_tapestry.nucleo.aplicacao.Usuario#login(br.ufes.scap_tapestry.entities.Pessoa)
-	 */
 	@Override
 	public void login(Pessoa usuario) {
 	    this.logado = usuario;
 	}
-	
-	/* (non-Javadoc)
-	 * @see br.ufes.scap_tapestry.nucleo.aplicacao.Usuario#isLogado()
-	 */
+
 	@Override
 	public boolean isLogado() {
 	    return logado != null;
 	}
 	
-	/* (non-Javadoc)
-	 * @see br.ufes.scap_tapestry.nucleo.aplicacao.Usuario#getMatricula()
-	 */
 	@Override
 	public String getMatricula(){
 		return logado.getMatricula();
 	}
 	
-	/* (non-Javadoc)
-	 * @see br.ufes.scap_tapestry.nucleo.aplicacao.Usuario#getLogado()
-	 */
 	@Override
 	public Pessoa getLogado(){
 		return logado;
 	}
 	
-	/* (non-Javadoc)
-	 * @see br.ufes.scap_tapestry.nucleo.aplicacao.Usuario#logout()
-	 */
 	@Override
 	public void logout(){
 		this.logado = null;
